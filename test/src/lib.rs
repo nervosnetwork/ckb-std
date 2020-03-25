bc7cf7181aad52c93533c16cdd8de0dae28a6fe9
jjy <jjyruby@gmail.com>
2020-03-25 16:46:34 +0800

    fix tests

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -1,10 +1,19 @@
 #[cfg(test)]
 mod tests {
-    use ckb_contract_tool::{bytes::Bytes, Context, TxBuilder};
+    use ckb_tool::{
+        bytes::Bytes,
+        ckb_types::{
+            core::ScriptHashType,
+            packed::{CellOutput, Script},
+            prelude::*,
+        },
+        testtool::{context::Context, tx_builder::TxBuilder},
+    };
     use std::fs::File;
     use std::io::Read;
 
-    #[test] fn it_works() {
+    #[test]
+    fn it_works() {
         let mut code = Vec::new();
         File::open("contract/target/riscv64imac-unknown-none-elf/release/contract")
             .unwrap()
@@ -13,8 +22,15 @@ mod tests {
         let code = Bytes::from(code);
         let mut context = Context::default();
         context.deploy_contract(code.clone());
+        let lock_code_hash = CellOutput::calc_data_hash(&code);
         let tx = TxBuilder::default()
-            .lock_bin(code)
+            .lock_script(
+                Script::new_builder()
+                    .code_hash(lock_code_hash)
+                    .hash_type(ScriptHashType::Data.into())
+                    .build()
+                    .as_bytes(),
+            )
             .inject_and_build(&mut context)
             .expect("build tx");
         println!("{:#x}", tx.hash());
