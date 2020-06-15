aea8c03b570cb96ba91b585b4b0cb51a03d66388
jjy <jjyruby@gmail.com>
2020-06-15 08:58:31 +0000

    Update docs

---
+++ docs/debug/build/ckb-std-a4831725336ea93e/build_script_build-a4831725336ea93e.d
@@ -0,0 +1,5 @@
+/code/docs/debug/build/ckb-std-a4831725336ea93e/build_script_build-a4831725336ea93e: build.rs
+
+/code/docs/debug/build/ckb-std-a4831725336ea93e/build_script_build-a4831725336ea93e.d: build.rs
+
+build.rs:
--- docs/riscv64imac-unknown-none-elf/doc/buddy_alloc/buddy_alloc/sidebar-items.js
+++ docs/riscv64imac-unknown-none-elf/doc/buddy_alloc/buddy_alloc/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MIN_LEAF_SIZE_ALIGN","required align to 16 bytes, since BuddyList takes 16 bytes on 64-bits machine."]],"fn":[["block_size",""],["first_up_k",""]],"struct":[["BuddyAlloc",""]]});
+initSidebarItems({"constant":[["MIN_LEAF_SIZE_ALIGN","required align to 16 bytes, since Node takes 16 bytes on 64-bits machine."]],"fn":[["block_size",""],["first_up_k",""]],"struct":[["BuddyAlloc",""]]});
