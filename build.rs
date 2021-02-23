2d465ed53eaedd4ba62d0909c29a6713e2b0b159
xjd <xjd@cryptape.com>
2021-02-23 13:13:40 +0800

    Remove dl-c-impl's Makefile and use build.rs instead
    follow Rust compiling workflow.

--- build.rs
+++ build.rs
@@ -2,18 +2,38 @@ use std::env;
 
 fn main() {
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+
+    let mut build = cc::Build::new();
+    build
+        .file("dl-c-impl/lib.c")
+        .static_flag(true)
+        .flag("-O3")
+        .flag("-fno-builtin-printf")
+        .flag("-fno-builtin-memcmp")
+        .flag("-nostdinc")
+        .flag("-nostdlib")
+        .flag("-fvisibility=hidden")
+        .flag("-fdata-sections")
+        .flag("-ffunction-sections")
+        .include("dl-c-impl/ckb-c-stdlib")
+        .include("dl-c-impl/ckb-c-stdlib/libc")
+        .flag("-Wall")
+        // .flag("-Werror")
+        .flag("-Wno-unused-parameter")
+        .flag("-Wno-nonnull")
+        .define("__SHARED_LIBRARY__", None);
     // ckb-std only supports riscv64 target arch
     // but we can still use cargo check under other archs
     if target_arch == "riscv64" {
         cc::Build::new()
             .file("src/asm/syscall.S")
             .compile("ckb-syscall");
-        // link against "dynamic loading" lib
-        println!("cargo:rustc-link-search=native=./dl-c-impl/build/");
-        println!("cargo:rustc-link-lib=static=dl-c-impl");
+        build.
+            flag("-Wno-nonnull-compare")
+            .flag("-nostartfiles")
+            .compile("dl-c-impl");
     } else {
-        // link against "dynamic loading" lib
-        println!("cargo:rustc-link-search=native=./dl-c-impl/build-x86/");
-        println!("cargo:rustc-link-lib=static=dl-c-impl");
+        build.define("CKB_STDLIB_NO_SYSCALL_IMPL", None);
+        build.compile("dl-c-impl");
     }
 }
