0361d476b1026c4666015c02f271a0b846065ccf
Linfeng Qian <thewawar@gmail.com>
2021-09-18 16:20:21 +0800

    test: assert argc/argv

--- test/contract/src/exec_callee.rs
+++ test/contract/src/exec_callee.rs
@@ -5,20 +5,21 @@
 #![feature(alloc_error_handler)]
 #![feature(panic_info_message)]
 
-use ckb_std::{cstr_core::CStr, debug, default_alloc, entry};
+use ckb_std::{
+    cstr_core::{cstr, CStr},
+    default_alloc, entry,
+};
 
 use core::slice::from_raw_parts;
 
 #[no_mangle]
 pub fn main(argc: u64, argv: *const *const u8) -> i8 {
-    debug!("argc: {}", argc);
     let args = unsafe { from_raw_parts(argv, argc as usize) };
-    for &arg in args {
-        debug!(
-            "arg: {:?}",
-            unsafe { CStr::from_ptr(arg) }.to_str().unwrap()
-        );
-    }
+    let arg1 = unsafe { CStr::from_ptr(args[0]) };
+    let arg2 = unsafe { CStr::from_ptr(args[1]) };
+    assert_eq!(argc, 2);
+    assert_eq!(arg1, cstr!("Hello World"));
+    assert_eq!(arg2, cstr!("你好"));
     0
 }
 
