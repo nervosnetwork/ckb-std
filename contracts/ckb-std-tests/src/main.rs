c660da768df85fa2b0fe78673278d49425ce6333
Yin Guanhao <sopium@mysterious.site>
2022-10-03 16:53:24 +0800

    asm_sym is no longer necessary

--- contracts/ckb-std-tests/src/main.rs
+++ contracts/ckb-std-tests/src/main.rs
@@ -6,7 +6,6 @@
 
 #![no_std]
 #![no_main]
-#![feature(asm_sym)]
 #![feature(lang_items)]
 #![feature(alloc_error_handler)]
 #![feature(panic_info_message)]
--- examples/main.rs
+++ examples/main.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(asm_sym)]
 #![feature(lang_items)]
 #![feature(alloc_error_handler)]
 #![feature(panic_info_message)]
