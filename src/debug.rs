3105c402649d1873968b057241acccd217c379dc
ian <me@iany.me>
2023-04-28 00:49:52 +0000

    chore: improve doc of the debug macro

--- src/debug.rs
+++ src/debug.rs
@@ -1,9 +1,20 @@
 /// debug macro
 ///
 /// Output a debug message.
-/// This macro only compiled under debug build and does nothing in release build.
 ///
-/// Notice: to see the debug output, you must turn on `ckb_script` debugging log in the CKB node configuration.
+/// This macro only compiled under debug build and does nothing in release build. To debug the release build,
+/// append the `--cfg debug_assertions` arguments to `cargo build`. For users of Capsule, the debug macro can
+/// be enabled in the release build by running `capsule build --release --debug-output`.
+///
+/// Notice: to see the debug output, you must turn on `ckb_script` debugging log in the CKB node configuration
+/// like this:
+///
+/// ```toml
+/// [logger]
+/// filter = "info,ckb-script=debug"
+/// ```
+/// 
+/// See the essay on more [Tips for Debugging CKB Scripts](https://docs.nervos.org/docs/essays/debug).
 ///
 /// # Example
 ///
