a855775a4d7cae1c30dac530741bc9f085604ec4
Xuejie Xiao <xxuejie@gmail.com>
2023-09-04 00:23:39 +0000

    Manually set optimization & debugging flags in clang

--- build.rs
+++ build.rs
@@ -11,7 +11,6 @@ fn main() {
         build
             .file("dl-c-impl/lib.c")
             .static_flag(true)
-            .flag("-O3")
             .flag("-fno-builtin-printf")
             .flag("-fno-builtin-memcmp")
             .flag("-nostdinc")
@@ -42,6 +41,17 @@ fn main() {
                 .no_default_flags(true)
                 .flag("--target=riscv64")
                 .flag("-march=rv64imc_zba_zbb_zbc_zbs");
+
+            if env::var("DEBUG").map(|v| v != "false").unwrap_or(false) {
+                build.flag("-g").flag("-fno-omit-frame-pointer");
+            }
+
+            let opt_level = env::var("OPT_LEVEL").expect("fetching OPT_LEVEL");
+            if opt_level == "z" {
+                build.flag("-Os");
+            } else {
+                build.flag(&format!("-O{}", opt_level));
+            }
         } else if compiler.is_like_gnu() {
             build
                 .flag("-nostartfiles")
