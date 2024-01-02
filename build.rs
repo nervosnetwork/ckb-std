fd046995bd499ffaba43310dbbdc8989d0146aff
Xuejie Xiao <xxuejie@gmail.com>
2024-01-02 07:21:05 +0000

    chore: Assorted cleanup work
    * Remove dummy-libc and rustc-dep-of-std features, since
    ckb-contract-toolchains has been deprecated, we are not using patched
    Rust with `target_os = ckb` anymore
    * dlopen-c is no longer a default feature, notice there is implication
    to this: dlopen-c as a feature used to help us bring in libc
    implementations. When simply removing dlopen-c, some contracts might
    break due to not having libc impls. This change adds another default
    feature `libc` which preserves a linked-by-default libc impl. This
    way, dlopen-c can be safely removed from default feature set

--- build.rs
+++ build.rs
@@ -1,68 +1,77 @@
 use std::env;
 
 fn main() {
+    println!("cargo:rerun-if-changed=c/dlopen.c");
+    println!("cargo:rerun-if-changed=c/libc.c");
+
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     // ckb-std only supports riscv64 target arch
     // but we can still use cargo check under other archs
     if target_arch == "riscv64" && cfg!(feature = "dlopen-c") {
         let mut build = cc::Build::new();
         build
-            .file("dl-c-impl/lib.c")
-            .static_flag(true)
-            .flag("-fno-builtin-printf")
-            .flag("-fno-builtin-memcmp")
-            .flag("-nostdinc")
-            .flag("-nostdlib")
-            .flag("-fvisibility=hidden")
-            .flag("-fdata-sections")
-            .flag("-ffunction-sections")
-            .include("dl-c-impl/ckb-c-stdlib")
-            .include("dl-c-impl/ckb-c-stdlib/libc")
-            .flag("-Wall")
-            .flag("-Werror")
-            .flag("-Wno-unused-parameter")
-            .flag("-Wno-nonnull")
-            .define("__SHARED_LIBRARY__", None);
-
-        if cfg!(feature = "build-with-clang") {
-            let clang = match std::env::var_os("CLANG") {
-                Some(val) => val,
-                None => "clang-16".into(),
-            };
-
-            build.compiler(clang);
-        }
-
-        let compiler = build.get_compiler();
-        if compiler.is_like_clang() {
-            build
-                .no_default_flags(true)
-                .flag("--target=riscv64")
-                .flag("-march=rv64imc_zba_zbb_zbc_zbs");
-
-            if env::var("DEBUG").map(|v| v != "false").unwrap_or(false) {
-                build.flag("-g").flag("-fno-omit-frame-pointer");
-            }
-
-            let opt_level = env::var("OPT_LEVEL").expect("fetching OPT_LEVEL");
-            if opt_level == "z" {
-                build.flag("-Os");
-            } else {
-                build.flag(&format!("-O{}", opt_level));
-            }
-        } else if compiler.is_like_gnu() {
-            build
-                .flag("-nostartfiles")
-                .flag("-Wno-dangling-pointer")
-                .flag("-Wno-nonnull-compare");
-        }
-
+            .file("c/dlopen.c")
+            .define("CKB_DECLARATION_ONLY", None);
+        setup_compiler_flags(&mut build);
         build.compile("dl-c-impl");
     }
 
-    if target_arch == "riscv64" && target_os == "ckb" && cfg!(feature = "dummy-libc") {
-        println!("cargo:rustc-link-lib=dummylibc");
+    if target_arch == "riscv64" && cfg!(feature = "libc") {
+        let mut build = cc::Build::new();
+        build.file("c/libc.c").define("__SHARED_LIBRARY__", None);
+        setup_compiler_flags(&mut build);
+        build.compile("libc");
+    }
+}
+
+fn setup_compiler_flags(build: &mut cc::Build) {
+    build
+        .static_flag(true)
+        .flag("-fno-builtin-printf")
+        .flag("-fno-builtin-memcmp")
+        .flag("-nostdinc")
+        .flag("-nostdlib")
+        .flag("-fvisibility=hidden")
+        .flag("-fdata-sections")
+        .flag("-ffunction-sections")
+        .include("c/ckb-c-stdlib")
+        .include("c/ckb-c-stdlib/libc")
+        .flag("-Wall")
+        .flag("-Werror")
+        .flag("-Wno-unused-parameter")
+        .flag("-Wno-nonnull");
+
+    let clang = match std::env::var_os("CLANG") {
+        Some(val) => val,
+        None => "clang-16".into(),
+    };
+
+    if cfg!(feature = "build-with-clang") {
+        build.compiler(clang);
+    }
+
+    let compiler = build.get_compiler();
+    if compiler.is_like_clang() {
+        build
+            .no_default_flags(true)
+            .flag("--target=riscv64")
+            .flag("-march=rv64imc_zba_zbb_zbc_zbs");
+
+        if env::var("DEBUG").map(|v| v != "false").unwrap_or(false) {
+            build.flag("-g").flag("-fno-omit-frame-pointer");
+        }
+
+        let opt_level = env::var("OPT_LEVEL").expect("fetching OPT_LEVEL");
+        if opt_level == "z" {
+            build.flag("-Os");
+        } else {
+            build.flag(&format!("-O{}", opt_level));
+        }
+    } else if compiler.is_like_gnu() {
+        build
+            .flag("-nostartfiles")
+            .flag("-Wno-dangling-pointer")
+            .flag("-Wno-nonnull-compare");
     }
 }
