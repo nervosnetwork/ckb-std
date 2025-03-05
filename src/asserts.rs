f83a45c0e6bf3918a2ffcf4229f2fe162b2d3b4d
Xuejie Xiao <xxuejie@gmail.com>
2025-03-05 08:32:25 +0000

    Restore default panic exit code after invocation

--- src/asserts.rs
+++ src/asserts.rs
@@ -1,6 +1,8 @@
 use core::fmt::Debug;
 
-pub static mut __PANIC_EXIT_CODE: i8 = -1;
+const DEFAULT_PANIC_EXIT_CODE: i8 = -1;
+
+pub static mut __PANIC_EXIT_CODE: i8 = DEFAULT_PANIC_EXIT_CODE;
 
 pub fn set_panic_exit_code(code: i8) {
     unsafe {
@@ -13,6 +15,7 @@ macro_rules! set_error_assert {
     ($code:expr, $($arg:tt)*) => {{
         $crate::asserts::set_panic_exit_code($code);
         assert!($($arg)*);
+        $crate::asserts::set_panic_exit_code(DEFAULT_PANIC_EXIT_CODE);
     }};
 }
 
@@ -21,10 +24,13 @@ macro_rules! set_error_assert_eq {
     ($code:expr, $($arg:tt)*) => {{
         $crate::asserts::set_panic_exit_code($code);
         assert_eq!($($arg)*);
+        $crate::asserts::set_panic_exit_code(DEFAULT_PANIC_EXIT_CODE);
     }};
 }
 
 pub fn set_error_expect<C: Into<i8>, T, E: Debug>(c: C, r: Result<T, E>, msg: &str) -> T {
     set_panic_exit_code(c.into());
-    r.expect(msg)
+    let t = r.expect(msg);
+    set_panic_exit_code(DEFAULT_PANIC_EXIT_CODE);
+    t
 }
